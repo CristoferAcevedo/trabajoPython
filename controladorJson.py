41c35df224bb342eca48d7ac89cbcc694fb1e72f
CristoferAcevedo <158364057+CristoferAcevedo@users.noreply.github.com>
2024-07-29 19:37:11 -0500

    controladorVersion2

--- controladorJson.py
+++ controladorJson.py
@@ -1,17 +1,28 @@
 import json
-#from modeloJson import modeloFormula
+from modelotrabajoAC import modeloFormula
 from vista import Vista
 class Controlador:
-    def __init__(self):
-        pass
+    def __init__(self,objModelo):
+        self.objModelo=objModelo
 
     def pasarDatos(self,datos):
         datosPersona=json.dumps(datos)
-        with open("baseDatos.txt", "w") as archivo:
-            json.dump(datosPersona,archivo)
+        self.objModelo.crearArchivo(datosPersona)
+
+    def enviarMostrar(self):
+        aux=self.objModelo.leerArchivo()
+        return aux
+
+    def enviarModificar(self,datos2):
+        datosPersona2=json.dumps(datos2)
+        self.objModelo.modificarArchivo(datosPersona2)
+
+    def enviarEliminar(self):
+        self.objModelo.eliminarArchivo()
 
 
-#objModelo=modeloFormula()
-objControlador=Controlador()
+
+objModelo=modeloFormula()
+objControlador=Controlador(objModelo)
 objVIsta=Vista(objControlador)
 objVIsta.crearVentana()
